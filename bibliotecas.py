abf90207978d313695c37430c4c36f5c98eaf708
Filipe Costa <filipe.rocha.costa12@gmail.com>
2025-11-25 09:27:37 -0100

    Update

---
+++ bibliotecas.py
@@ -0,0 +1,5 @@
+import cv2              # vídeo e imagens
+import os               # manipulação de arquivos e diretórios
+import json             # leitura e escrita de arquivos JSON
+import numpy as np      # manipulação de arrays
+import time             # controle de tempo e delays
